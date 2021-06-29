f4e0dabb1b5b02dd9d7b59a9172d1fcf02134430
davisanc <dsanchezrodriguez@gmail.com>
2021-06-29 15:28:57 +0100

    added NSG to NIC

--- createVM.tf
+++ createVM.tf
@@ -5,7 +5,23 @@ resource "azurerm_public_ip" "public-pip-example" {
   allocation_method   = "Static"
   sku                 = "Standard"
 }
+resource "azurerm_network_security_group" "nsg" {
+  name                = "vm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
+}
 resource "azurerm_network_interface" "example" {
   name                = "vm-nic"
   location            = azurerm_resource_group.rg.location
@@ -19,7 +35,10 @@ resource "azurerm_network_interface" "example" {
 
   }
 }
-
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 resource "random_id" "password" {
   byte_length = 4
 }
