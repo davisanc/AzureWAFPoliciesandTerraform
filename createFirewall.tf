cc2eeb465158e0c4c95eee489d17d289ca560b27
davisanc <dsanchezrodriguez@gmail.com>
2021-07-01 16:18:48 +0100

    change app rule

--- createFirewall.tf
+++ createFirewall.tf
@@ -63,8 +63,8 @@ resource "azurerm_firewall_policy_rule_collection_group" "example" {
         type = "Https"
         port = 443
       }
-      source_addresses  = ["10.0.0.1"]
-      destination_fqdns = [".microsoft.com"]
+      source_addresses  = ["*"]
+      destination_fqdns = ["*"]
     }
   }
 
