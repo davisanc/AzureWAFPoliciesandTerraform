357ff20e776c9f9962bbdacf9dcd7cc9e163786a
davisanc <dsanchezrodriguez@gmail.com>
2021-07-01 16:02:50 +0100

    nat rule hardcoded

--- createFirewall.tf
+++ createFirewall.tf
@@ -91,7 +91,8 @@ resource "azurerm_firewall_policy_rule_collection_group" "example" {
       source_addresses    = ["*"]
       destination_address = azurerm_firewall.firewall.ip_configuration[0].public_ip_address_id
       destination_ports   = ["80"]
-      translated_address  = azurerm_private_endpoint.privateendpoint.private_service_connection[0].private_ip_address
+      //translated_address  = azurerm_private_endpoint.privateendpoint.private_service_connection[0].private_ip_address
+      translated_address  = "10.1.3.4"
       translated_port     = "80"
     }
   }
