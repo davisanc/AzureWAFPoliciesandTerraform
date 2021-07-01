a3f3322c99dbec3f35bc626f72eee1da478635f1
davisanc <dsanchezrodriguez@gmail.com>
2021-07-01 13:09:56 +0100

    change

--- createFirewall.tf
+++ createFirewall.tf
@@ -30,7 +30,7 @@ resource "azurerm_firewall" "firewall" {
   }
 }
 
-//adding firewall policy!!
+//adding firewall policy to firewall
 resource "azurerm_firewall_policy" "fwpolicy" {
   name                = "fwpolicy"
   resource_group_name = azurerm_resource_group.rg.name
