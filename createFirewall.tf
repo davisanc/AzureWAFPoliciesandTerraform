f5ad3ee6a1a5cc38419ad9ba8025b37d5dd970b6
davisanc <dsanchezrodriguez@gmail.com>
2021-07-26 09:56:41 +0100

    change nat rule

--- createFirewall.tf
+++ createFirewall.tf
@@ -76,11 +76,11 @@ resource "azurerm_firewall_policy_rule_collection_group" "example" {
       name                = "nat_rule_collection1_rule1"
       protocols           = ["TCP"]
       source_addresses    = ["*"]
-      //destination_address = azurerm_firewall.firewall.ip_configuration[0].public_ip_addressses
-      destination_address = "20.90.240.234"
+      destination_address = azurerm_firewall.firewall.ip_configuration[0].public_ip_addressses
+      //destination_address = "20.90.240.234"
       destination_ports   = ["80"]
-      //translated_address  = azurerm_private_endpoint.privateendpoint.private_service_connection[0].private_ip_address
-      translated_address  = "10.1.3.4"
+      translated_address  = azurerm_private_endpoint.privateendpoint.private_service_connection[0].private_ip_address
+      //translated_address  = "10.1.3.4"
       translated_port     = "80"
     }
     rule {
