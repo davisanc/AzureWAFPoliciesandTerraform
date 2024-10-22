c40a7fc2b594b9279271014cf5a9ed05617b7f75
David Sanchez <dasanc@microsoft.com>
2024-10-22 14:30:06 +0100

    block access to RDP on vm

--- createVM.tf
+++ createVM.tf
@@ -13,7 +13,7 @@ resource "azurerm_network_security_group" "nsg" {
     name                       = "AllowRDP"
     priority                   = 100
     direction                  = "Inbound"
-    access                     = "Allow"
+    access                     = "Deny"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3389"
