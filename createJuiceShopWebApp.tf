87a159ea6a2748f61d46aaab996f3316b2f43c6f
David Sanchez <dasanc@microsoft.com>
2024-11-06 11:38:38 +0000

    build app for demo

--- createJuiceShopWebApp.tf
+++ createJuiceShopWebApp.tf
@@ -14,7 +14,7 @@ resource "azurerm_app_service_plan" "example" {
 resource "random_id" "webappname" {
   byte_length = 2
 }
-#create web app service for juiceshop
+#create web app service for juiceshop and deploy
 resource "azurerm_app_service" "appservice" {         
   name                = "juiceshop${random_id.webappname.hex}"
   location            = azurerm_resource_group.rg.location
